b47e12a55f46e556784495796fafd63d22fbccb0
chethan.m <chethan.m@arithaconsulting.com>
2024-02-09 11:34:22 +0530

    refactor: code refactor according to new editor

--- src/components/HeroBrikTwo.jsx
+++ src/components/HeroBrikTwo.jsx
@@ -1,34 +1,37 @@
-import { Button, Box, Title,Text} from "@apf/core";
+import { Button, Title,Text} from "@apf/core";
 import React from "react";
 import "./HeroBrikTwo.css";
 // import IconSearch from "react-icons";
 function HeroBrikTwo(props){
     // eslint-disable-next-line react/prop-types
-        const backgroundImg = props.data.data.find((item) => item.content.type === "image" && item.content.for === "background");
+        const backgroundImg = props?.data?.data?.find((item) => item?.content?.type === "img" && item?.content?.for === "background");
         // eslint-disable-next-line react/prop-types
-        const titleObj = props.data.data.find((item) => item.content.type === "title");
+        const titleObj = props?.data?.data?.find((item) => item?.content?.type === "title" && item?.content?.for === "Main Title");
         // eslint-disable-next-line react/prop-types
-        const textOneObj = props.data.data.find((item) => item.content.type ==="textOne");
+        const textOneObj = props?.data?.data.find((item) => item?.content?.type ==="subtext" && item?.content?.for === "subtext-a");
         // eslint-disable-next-line react/prop-types
-        const textTwoObj = props.data.data.find((item) => item.content.type ==="textTwo");
+        const textTwoObj = props.data.data.find((item) => item.content.type ==="subtext" && item?.content?.for === "subtext-b");
         // eslint-disable-next-line react/prop-types
         const buttonObj = props.data.data.find((item) => item.content.type === "button");
     return( 
     <div className="hero-brick-container">
-        <Box>
-            <img src={backgroundImg.content.link}  className="HeroImageDiv"></img>
-            <div className="hero-content-container">
-                <Text c={textOneObj.style.font.color} fz={textOneObj.style.font.size} style={{ fontFamily: textOneObj.style.font.family }} fs={textOneObj.style.font.style} fw={textOneObj.style.font.weight} className="hero-subtitle-one">{textOneObj.content.value}</Text>
-                <Title c={titleObj.style.font.color} fz={titleObj.style.font.size}  style={{ fontFamily: titleObj.style.font.family }} fw={titleObj.style.font.weight} className="hero-title">{titleObj.content.value}</Title>
-                <Text c={textTwoObj.style.font.color} fz={textTwoObj.style.font.size} style={{ fontFamily: textTwoObj.style.font.family }} fs={textTwoObj.style.font.style} fw={textTwoObj.style.font.weight}className="hero-subtitle-two">{textTwoObj.content.value}</Text>
-                <div className="Parent-Button">
-                    <Button variant="filled" style={{ color: buttonObj.style.font.color, backgroundColor: buttonObj.style.backgroundColor}} size={buttonObj.style.font.size} radius="xl" className="hero-button">{buttonObj.content.value}</Button>
-                </div>
-                
+            <div className="hero-image-container">
+                <img src={backgroundImg?.content?.src}  className="HeroImageDiv"></img>
             </div>
-            
-      
-      </Box>
+            <div className="hero-content-container"> 
+                <div>
+                    <Text c={textOneObj.style.font.color} fz={textOneObj.style.font.size} style={{ fontFamily: textOneObj.style.font.family }} fs={textOneObj.style.font.style} fw={textOneObj.style.font.weight} className="hero-subtitle-one">{textOneObj.content.value}</Text> 
+                </div>
+                <div className="hero-title">
+                    <Title c={titleObj.style.font.color} fz={titleObj.style.font.size}  style={{ fontFamily: titleObj.style.font.family }} fw={titleObj.style.font.weight} className="title-tag">{titleObj.content.value}</Title>
+                </div>
+                <div>
+                    <Text c={textTwoObj.style.font.color} fz={textTwoObj.style.font.size} style={{ fontFamily: textTwoObj.style.font.family }} fs={textTwoObj.style.font.style} fw={textTwoObj.style.font.weight}className="hero-subtitle-two">{textTwoObj.content.value}</Text>
+                </div>
+                <div className="Parent-Button">
+                    <Button variant="filled" style={{ color: buttonObj.style.font.color, backgroundColor: buttonObj.style.backgroundColor , height: buttonObj?.style?.height, width: buttonObj?.style?.width}} size={buttonObj.style.font.size} radius="xl" className="hero-button">{buttonObj.content.value}</Button>
+                </div>
+            </div>
     </div>
     );
 
