0b38a545c19e6d0d3c04cb478d92b1590d34c5c3
chethan.m <chethan.m@arithaconsulting.com>
2024-03-15 12:07:54 +0530

    chore: refactor of code

--- src/components/HeroBrikTwo.jsx
+++ src/components/HeroBrikTwo.jsx
@@ -1,37 +1,36 @@
-import { Button, Title,Text} from "@apf/core";
+/* eslint-disable react/prop-types */
+import {Title,Text,Button} from "@apf/core";
 import React from "react";
 import "./HeroBrikTwo.css";
-// import IconSearch from "react-icons";
+
 function HeroBrikTwo(props){
-    // eslint-disable-next-line react/prop-types
         const backgroundImg = props?.data?.data?.find((item) => item?.content?.type === "img" && item?.content?.for === "background");
-        // eslint-disable-next-line react/prop-types
         const titleObj = props?.data?.data?.find((item) => item?.content?.type === "title" && item?.content?.for === "Main Title");
-        // eslint-disable-next-line react/prop-types
-        const textOneObj = props?.data?.data.find((item) => item?.content?.type ==="subtext" && item?.content?.for === "subtext-a");
-        // eslint-disable-next-line react/prop-types
-        const textTwoObj = props.data.data.find((item) => item.content.type ==="subtext" && item?.content?.for === "subtext-b");
-        // eslint-disable-next-line react/prop-types
-        const buttonObj = props.data.data.find((item) => item.content.type === "button");
-    return( 
-    <div className="hero-brick-container">
-            <div className="hero-image-container">
+        const textTwoObj = props?.data?.data?.find((item) => item?.content?.type ==="subtext" && item?.content?.for === "subtext-b");
+        const buttonObj = props?.data?.data?.find((item) => item?.content?.type === "button");
+        const brikImage = props?.data?.data?.find((item) => item?.content?.type === "img" && item?.content?.for === "brikImage");
+
+        const handleButtonClick = () => {
+            window.open(buttonObj?.content?.link, buttonObj?.content?.target);
+          };
+        return( 
+    <div className="hero-brik-container" style={{backgroundColor: props?.data?.style?.backgroundColor , height:props?.data?.style?.height}}>
+            {backgroundImg && backgroundImg?.display === "block" &&<div className="hero-image-container" >
                 <img src={backgroundImg?.content?.src}  className="HeroImageDiv"></img>
-            </div>
-            <div className="hero-content-container"> 
-                <div>
-                    <Text c={textOneObj.style.font.color} fz={textOneObj.style.font.size} style={{ fontFamily: textOneObj.style.font.family }} fs={textOneObj.style.font.style} fw={textOneObj.style.font.weight} className="hero-subtitle-one">{textOneObj.content.value}</Text> 
+            </div>}
+                <div className={brikImage?.display === "block" ? "hero-content-container" : "hero-content-container-1"}> 
+                    {titleObj && titleObj?.display === "block" &&<div className="hero-title">
+                        <Title c={titleObj?.style?.font?.color} fz={titleObj?.style?.font?.size}  style={{ fontFamily: titleObj?.style?.font?.family }} fw={titleObj?.style?.font?.weight} className="title-tag">{titleObj?.content?.value}</Title>
+                    </div>}
+                    {textTwoObj && textTwoObj?.display === "block" &&<div className="hero-subtitle">
+                        <Text c={textTwoObj?.style?.font?.color} fz={textTwoObj?.style?.font?.size} style={{ fontFamily: textTwoObj?.style?.font?.family }} fs={textTwoObj?.style?.font?.style} fw={textTwoObj?.style?.font?.weight}className="hero-subtitle-two">{textTwoObj?.content?.value}</Text>
+                    </div>}
+                    {buttonObj && buttonObj?.display === "block" &&<div className="Parent-Button">
+                        <Button variant="filled" style={{ color: buttonObj?.style?.font?.color, backgroundColor: buttonObj?.style?.backgroundColor , height: buttonObj?.style?.height, width: buttonObj?.style?.width, radius:buttonObj?.style?.radius}} size={buttonObj?.style?.font?.size} onClick={handleButtonClick} className="hero-button">{buttonObj?.content?.value}</Button>
+                    </div>}
+                    {brikImage && brikImage?.display === "block" &&<div className={buttonObj?.display === "block" ? "hero-brikImage-container" : "hero-brikImage-container-1"} >
+                    <img src={brikImage?.content?.src}  className="brikImageDiv"></img></div>}
                 </div>
-                <div className="hero-title">
-                    <Title c={titleObj.style.font.color} fz={titleObj.style.font.size}  style={{ fontFamily: titleObj.style.font.family }} fw={titleObj.style.font.weight} className="title-tag">{titleObj.content.value}</Title>
-                </div>
-                <div>
-                    <Text c={textTwoObj.style.font.color} fz={textTwoObj.style.font.size} style={{ fontFamily: textTwoObj.style.font.family }} fs={textTwoObj.style.font.style} fw={textTwoObj.style.font.weight}className="hero-subtitle-two">{textTwoObj.content.value}</Text>
-                </div>
-                <div className="Parent-Button">
-                    <Button variant="filled" style={{ color: buttonObj.style.font.color, backgroundColor: buttonObj.style.backgroundColor , height: buttonObj?.style?.height, width: buttonObj?.style?.width}} size={buttonObj.style.font.size} radius="xl" className="hero-button">{buttonObj.content.value}</Button>
-                </div>
-            </div>
     </div>
     );
 
